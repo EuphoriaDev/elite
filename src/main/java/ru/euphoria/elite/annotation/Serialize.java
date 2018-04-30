cfe580014c5736a628a99250faab173772e47981
admin <admin@Mac-mini-admin.local>
2018-05-01 01:28:14 +0400

    support for serialize object and get cursor from query

---
+++ src/main/java/ru/euphoria/elite/annotation/Serialize.java
@@ -0,0 +1,8 @@
+package ru.euphoria.elite.annotation;
+
+/**
+ * Created by admin on 30.04.18.
+ */
+
+public @interface Serialize {
+}
---
+++ src/main/java/ru/euphoria/elite/util/BlobUtils.java
@@ -0,0 +1,8 @@
+package ru.euphoria.elite.util;
+
+/**
+ * Created by admin on 30.04.18.
+ */
+
+public class BlobUtils {
+}
